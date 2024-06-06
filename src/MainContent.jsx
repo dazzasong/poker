d7626494c25d8f86b4780fe2e753e7ac1fecb906
Darren Song <dazzasong@gmail.com>
2024-06-06 11:59:24 +1000

    Added chair, modified style

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,23 +2,47 @@ import React from "react";
 import { Box, Button, Slider, Stack, Typography } from "@mui/material";
 import WalletIcon from '@mui/icons-material/Wallet';
 import PriceCheckIcon from '@mui/icons-material/PriceCheck';
+import chair from './assets/audio/chair.mp3';
 import kaching from './assets/audio/kaching.mp3';
 
+const chairSfx = new Audio(chair);
 const kachingSfx = new Audio(kaching);
 
 export default function MainContent() {
   const [gameState, setGameState] = React.useState(0);
   const [balance, setBalance] = React.useState(25);
+  const [bValue, setBValue] = React.useState(5);
   const [bet, setBet] = React.useState(0);
-  if (gameState === 1) {
-    // do something
-  } else if (gameState === 2 ) {
-    // do something
+  
+  let bValueStatus = 'green';
+  
+  if (!bet) {
+    if (balance < bValue) bValueStatus = 'red';
+    else if (balance === bValue) bValueStatus = 'orange';
+    else bValueStatus = 'green';
   }
-  function confirmBet() {
-    setBet(100);
-    kachingSfx.play()
+
+  React.useEffect(() => {
+    if (gameState === 1) {
+      chairSfx.play();
+    } else if (gameState === 2) {
+      //do
+    } else {
+      setBet(0);
+    }
+  }, [gameState]);
+
+  const handleChange = (event, newValue) => setBValue(newValue);
+  const confirmBet = () => {
+    if (balance < bValue) {
+      window.alert('Not enough balance!');
+    } else {
+      setBet(bValue);
+      setBalance(balance - bValue);
+      kachingSfx.play();
+    }
   }
+
   return (
     <Box>
       <Stack direction='row' spacing={0.5}>
@@ -27,31 +51,51 @@ export default function MainContent() {
           ${balance}K
         </Typography>
       </Stack>
-      <Stack alignItems='center'>
-        <Typography fontSize={20} fontWeight='bold'>
-          Welcome to the poker table! Grab a seat and place your bets!
-        </Typography>
-        <Stack direction='row' width={200}>
-          <Slider
-            valueLabelDisplay="auto"
-            min={10}
-            step={10}
-          />
-          <Button variant="contained" color="success" onClick={confirmBet}>BET</Button>
-          { bet &&
-            <PriceCheckIcon color="success" />
-          }
+      { gameState === 0 &&
+        <Stack alignItems='center'>
+          <Typography fontSize={20}>
+            Welcome to the poker table! Grab a seat and place your bets!
+          </Typography>
+          <Stack direction='row' spacing={2}>
+            <Box width={200}>
+              <Slider
+                color="success"
+                min={5}
+                step={5}
+                disabled={bet}
+                onChange={handleChange}
+              />
+            </Box>
+            <Typography color={bValueStatus} fontSize={20}>
+              {bValue}K
+            </Typography>
+            <Button
+              variant="contained"
+              color="success"
+              disabled={bet}
+              onClick={confirmBet}
+            >
+              Bet
+            </Button>
+            { bet > 0 &&
+              <PriceCheckIcon color="success" />
+            }
+          </Stack>
+        { bet > 0 &&
+          <Stack justifyContent='center' alignItems='center' height={400}>
+            <Typography
+                color='gold'
+                fontSize={25}
+                fontWeight='bold'
+                sx={{ textShadow: '2px 2px goldenrod' }}
+              >
+                Ready to PLAY?
+              </Typography>
+              <Button variant="contained" color="warning" onClick={() => setGameState(1)}>Yes</Button>
+          </Stack>
+        }
         </Stack>
-        <Typography
-          color='gold'
-          fontSize={25}
-          fontWeight='bold'
-          sx={{ textShadow: '2px 2px goldenrod' }}
-        >
-          Ready to PLAY?
-        </Typography>
-        <Button variant="contained" color="warning" onClick={() => setGameState(1)}>Yes</Button>
-      </Stack>
+      }
     </Box>
   )
 }

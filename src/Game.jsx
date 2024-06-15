7e1d9157dd81a52e9a60245e2b14a8ab0ab1deac
Darren Song <dazzasong@gmail.com>
2024-06-15 10:20:15 +1000

    Added cardDeck and drawCards function

--- src/Game.jsx
+++ src/Game.jsx
@@ -57,6 +57,26 @@ import blackJoker from './assets/images/black_joker.png';
 import redJoker from './assets/images/red_joker.png';
 
 export default function Game() {
+  let cardDeck = [
+    -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27
+  ]; // Negatives are black, positives are red. First half is clubs or diamonds - second half is spades or hearts. 1s are aces and 27s are jokers.
+  function drawCards(numberOfCards) {
+    let drawnCards = new Set();
+    while (drawnCards.size < numberOfCards) {
+      const randomIndex = Math.floor(Math.random() * cardDeck.length);
+      const randomCard = cardDeck[randomIndex];
+      drawnCards.add(randomCard);
+    }
+    return drawnCards;
+  }
+  function CardsInHand() {
+    return (
+      <Stack>
+        
+      </Stack>
+    );
+  }
   return (
     <Stack height='100vh'>
       <Typography>
